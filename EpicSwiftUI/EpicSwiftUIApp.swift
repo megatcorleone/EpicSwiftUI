224ac93dc69a59ee7f6eb84d328a66435f6a3f85
Megat Corleone <megatcorleone000@gmail.com>
2024-08-22 11:33:43 +0800

    Initial Commit

---
+++ EpicSwiftUI/EpicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpicSwiftUIApp.swift
+//  EpicSwiftUI
+//
+//  Created by N7168 on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct EpicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
