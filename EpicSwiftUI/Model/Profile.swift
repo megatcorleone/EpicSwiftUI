5d10c77dcff019747ef0595d9f4f82f241e7a9b8
Megat Corleone <megatcorleone000@gmail.com>
2024-08-23 14:40:07 +0800

    Megat - Completed UI

---
+++ EpicSwiftUI/Model/Profile.swift
@@ -0,0 +1,29 @@
+//
+//  Profile.swift
+//  EpicSwiftUI
+//
+//  Created by N7168 on 23/08/2024.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+
+    static let `default` = Profile(username: "megatcorleone")
+
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+
+        var id: String { rawValue }
+    }
+}
