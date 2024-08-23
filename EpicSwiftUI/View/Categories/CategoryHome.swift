6a36fb4b267a2527e0fcf4cfc512c3aff0db7525
Megat Corleone <megatcorleone000@gmail.com>
2024-08-23 11:05:58 +0800

    Megat - Commit Tabview UI

---
+++ EpicSwiftUI/View/Categories/CategoryHome.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryHome.swift
+//  EpicSwiftUI
+//
+//  Created by N7168 on 23/08/2024.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
